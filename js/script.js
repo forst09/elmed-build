e8bb64bb32a01a10cf23f9ea669f4e6644c0d4d5
forst09 <dforst09@gmail.com>
2022-10-31 12:27:12 +0300

    add about

--- js/script.js
+++ js/script.js
@@ -183,6 +183,30 @@ $(document).ready(function () {
         }
     };
 
+    //ОТКРЫТИЕ ВИДЕО С СЕКЦИИ О НАС
+    $(document).on('click', '.about__video-play', function (e) {
+        e.preventDefault();
+        let firstString = 'https://www.youtube.com/embed/';
+        let link = $(this).attr('href');
+        let newLink;
+        if (link.indexOf('v=') !== -1) {
+            let arrayLinks = link.split('v=');
+            let secondString = arrayLinks[arrayLinks.length - 1];
+            newLink = firstString + secondString;
+        }
+        else {
+            newLink = link;
+        }
+
+        const fancybox = Fancybox.show([
+            {
+                src: newLink,
+                type: 'iframe'
+            }
+        ])
+
+    });
+
     // ПОДКЛЮЧЕНИЕ КАРТЫ
     let isMapLoaded = false;
     const jsMap = document.querySelector("#map");
