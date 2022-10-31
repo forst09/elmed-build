dcbb27018e80206a7fba69b58ad506ecdef6fbed
forst09 <dforst09@gmail.com>
2022-10-31 12:18:30 +0300

    add advantages

--- js/script.js
+++ js/script.js
@@ -161,4 +161,25 @@ $(document).ready(function () {
             $(this).parents('.input-choice__wrapper').removeClass('active');
         }
     });
+
+    //ЭЛЕМЕНТЫ СПИСКА ВЫЕЗЖАЮТ СЛЕВА НА МОБИЛКЕ
+    if ($(window).width() < 668) {
+        function onEntry(entry) {
+            entry.forEach(change => {
+                if (change.isIntersecting) {
+                    change.target.classList.add('element-show');
+                }
+            });
+        }
+
+        let options = {
+            threshold: [0.3]
+        };
+        let observer = new IntersectionObserver(onEntry, options);
+        let elements = document.querySelectorAll('.advantages__text-item');
+
+        for (let elm of elements) {
+            observer.observe(elm);
+        }
+    }
 });
