551680554827dedb9e6c9f34eac7582ad6d0930e
forst09 <dforst09@gmail.com>
2022-10-31 12:51:29 +0300

    dropdown

--- js/script.js
+++ js/script.js
@@ -26,6 +26,12 @@ $(document).ready(function () {
         }
     });
 
+    //РАСКРЫТИЕ ДРОПДАУНОВ
+    $(document).on('click', ".dropdown__link", function () {
+        $(this).toggleClass("active").parent().siblings().find(".dropdown__link").removeClass("active");
+        $(this).parent().find('.dropdown__content').slideToggle().parent().siblings().find(".dropdown__content").slideUp();
+    });
+
     // ПОКАЗАТЬ/СКРЫТЬ ТЕКСТ
     $(document).on('click', ".btn-show", function () {
         if ($(this).hasClass('active')) {
